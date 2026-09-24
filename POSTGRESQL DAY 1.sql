c7bdff2ef7bcd795a8c375e766ccac0d6fbc3f37
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-25 03:25:09 +0530

    primary key ,not null and default value

--- POSTGRESQL DAY 1.sql
+++ POSTGRESQL DAY 1.sql
@@ -1,5 +1,5 @@
-SELECT * FROM person;
-
-INSERT INTO person(id,name)
-Values
-(101,'Alex');
+Create table customers(
+acc_no INT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+acc_type VARCHAR(50) NOT NULL DEFAULT 'Savings'
+)
